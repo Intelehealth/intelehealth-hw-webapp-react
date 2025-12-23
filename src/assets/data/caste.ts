ad2c109b3313195b2e3a97550c851efb11c76434
AkshayIntelehealth <akshay@intelehealth.org>
2025-12-23 10:17:00 +0530

    fix: dropdown values updated

---
+++ src/assets/data/caste.ts
@@ -0,0 +1,8 @@
+// caste.ts
+export const casteOptions = [
+  { label: 'General', value: 'General' },
+  { label: 'OBC', value: 'OBC' },
+  { label: 'SC', value: 'SC' },
+  { label: 'ST', value: 'ST' },
+  { label: 'Others', value: 'Others' },
+];
---
+++ src/assets/data/occupation.ts
@@ -0,0 +1,14 @@
+// occupation.ts
+export const occupationOptions = [
+  { label: 'Salaried Employee', value: 'Salaried Employee' },
+  { label: 'Business Owner', value: 'Business Owner' },
+  {
+    label: 'Technician or Craftsman/Other Skilled Work (Driver, Mason etc)',
+    value: 'Technician or Craftsman/Other Skilled Work (Driver, Mason etc)',
+  },
+  { label: 'Agricultural Farmer', value: 'Agricultural Farmer' },
+  { label: 'Homemaker', value: 'Homemaker' },
+  { label: 'Student', value: 'Student' },
+  { label: 'Unemployed', value: 'Unemployed' },
+  { label: 'Others (Specify)', value: 'Others (Specify)' },
+];
