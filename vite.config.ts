2903992899f3994dd862995d53569a60509c7935
shaheen-p <spathan@intelehealth>
2026-02-02 15:49:04 +0530

    feat: updated vites.config to exclude the debug-logger

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     target: 'es2020',
     minify: 'esbuild',
-    sourcemap: true, // Enable sourcemaps for debugging in production
+    sourcemap: false, // Enable sourcemaps for debugging in production
     rollupOptions: {
       output: {
         manualChunks: {
